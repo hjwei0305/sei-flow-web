d1abc0500c8ebd8c95e59c28c4478ddbb79c74d0
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-03-04 22:32:45 +0800

    测试14

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,7 @@ import request from './request';
 import * as commonUtils from './common';
 import * as CommonComponentsConfig from './CommonComponentsConfig';
 import storage from './storage';
-
+console.log(constants, 'constants');
 export {
   constants,
   userUtils,
