bdbd26a3ce85f231d1875297001f40a97a7b0f61
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-03-04 22:19:25 +0800

    测试12

--- src/pages/AppModule/AppModuleService.js
+++ src/pages/AppModule/AppModuleService.js
@@ -1,16 +1,18 @@
 import { request, constants, } from "@/utils";
-import {findAllByAuth} from "../FlowType/FlowTypeService";
 console.log(constants);
-const { baseUrl } = constants;
+// const { baseUrl } = constants;
 export async function getAllList(params = {}) {
-    return request.get(baseUrl + "/appModule/findAll", params);
+    // return request.get(baseUrl + "/appModule/findAll", params);
+    return request.get("/appModule/findAll", params);
 }
 
 export async function findAllAppModuleByAuth(params = {}) {
-    return request.get(baseUrl + "/appModule/findAllByAuth", params);
+    // return request.get(baseUrl + "/appModule/findAllByAuth", params);
+    return request.get("/appModule/findAllByAuth", params);
 }
 export async function save(params = {}) {
-    return request.postJson(baseUrl + "/appModule/save",JSON.stringify(params));
+    // return request.postJson(baseUrl + "/appModule/save",JSON.stringify(params));
+    return request.postJson("/appModule/save",JSON.stringify(params));
 }
 
 /**
@@ -19,6 +21,7 @@ export async function save(params = {}) {
  * return 操作结果
  */
 export async function deleteCorp(param=''){
-    return request.delete(baseUrl+"/appModule/delete",param);
+    return request.delete("/appModule/delete",param);
+    // return request.delete(baseUrl+"/appModule/delete",param);
 }
 
