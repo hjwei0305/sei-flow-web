a01ed35b6d82f8c8f3f5d4e2101aecdbf46d0c32
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-03-06 11:33:05 +0800

    传递参数模式

--- public/flow/scripts/design/ChooseUserView.js
+++ public/flow/scripts/design/ChooseUserView.js
@@ -78,7 +78,7 @@ EUI.ChooseUserView = EUI.extend(EUI.CustomUI, {
     });
     EUI.Store({
       url:  _ctxPath + "/flowTask/getExecutorsByExecutorsVos",
-      data: {
+      postParam: {
         requestExecutorsVos: JSON.stringify(requestExecutorsVos),
         businessModelCode: this.businessModelCode,
         businessId: this.businessId
