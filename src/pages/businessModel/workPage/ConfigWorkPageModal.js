3cb32c9e7cdd3d8652cca55a02985d6475816cc7
何灿坤 <cankun.he@changhong.com>
2020-11-16 12:07:14 +0800

    业务实体配置工作界面添加搜索框

--- src/pages/businessModel/workPage/ConfigWorkPageModal.js
+++ src/pages/businessModel/workPage/ConfigWorkPageModal.js
@@ -5,152 +5,159 @@
 
 import React, {Component} from 'react'
 import {connect} from 'dva';
-import {Modal} from 'antd';
+import {Input, Modal} from 'antd';
 import TransferTable from "@/components/TransferTable";
 import {listAllNotSelectEdByAppModuleId, listAllSelectEdByAppModuleId, saveSetWorkPage} from "../BusinessModelService";
-import { seiLocale } from 'sei-utils';
-import { appModuleConfig, } from '@/utils/CommonComponentsConfig';
+import {seiLocale} from 'sei-utils';
+import {appModuleConfig,} from '@/utils/CommonComponentsConfig';
 
-const { seiIntl } = seiLocale;
+const {seiIntl} = seiLocale;
+
 class ConfigWorkPageModal extends Component {
 
-    //删除分配
-    handleLeftClick = async (rows, rightData) => {
-        // console.log("left:",rows,"right:",rightData)
-        const {appModuleId, businessModelId} = this.props;
-        let ids = [];
-        for (let data of rightData) {
-            if (rows.findIndex(item => item.id === data.id) > -1) {
-                continue;
-            }
-            ids.push(data.id);
-        }
-        await saveSetWorkPage(`/${businessModelId}`, ids.join(',')).then((data) => {
-        }).catch(err => {
-            this.toggoleGlobalLoading(false)
-        })
-
+  //删除分配
+  handleLeftClick = async (rows, rightData) => {
+    const {appModuleId, businessModelId} = this.props;
+    let ids = [];
+    for (let data of rightData) {
+      if (rows.findIndex(item => item.id === data.id) > -1) {
+        continue;
+      }
+      ids.push(data.id);
     }
+    await saveSetWorkPage(`/${businessModelId}`, ids.join(',')).then((data) => {
+    }).catch(err => {
+      this.toggoleGlobalLoading(false)
+    })
 
-    toggoleGlobalLoading = (loading) => {
-      const { dispatch, } = this.props;
-      dispatch({
-        type: 'global/updateState',
-        payload: {
-          globalLoading: loading,
-        }
-      });
+  }
+
+  toggoleGlobalLoading = (loading) => {
+    const {dispatch,} = this.props;
+    dispatch({
+      type: 'global/updateState',
+      payload: {
+        globalLoading: loading,
+      }
+    });
+  }
+
+  //插入分配
+  handleRightClick = async (rows, rightData) => {
+    const {appModuleId, businessModelId} = this.props;
+    let ids = [];
+    for (let i = 0; i < rows.length; i++) {
+      ids.push(rows[i].id);
     }
-
-    //插入分配
-    handleRightClick = async (rows, rightData) => {
-        // console.log("left:",rows,"right:",rightData)
-        const {appModuleId, businessModelId} = this.props;
-        let ids = [];
-        for (let i = 0; i < rows.length; i++) {
-            ids.push(rows[i].id);
-        }
-        for (let data of rightData) {
-            if (rows.findIndex(item => item.id === data.id) > -1) {
-                continue;
-            }
-            ids.push(data.id);
-        }
-        await saveSetWorkPage(`/${businessModelId}`, ids.join(',')).then((data) => {
-        }).catch(err => {
-            this.toggoleGlobalLoading(false)
-        })
-    };
-
-    leftService = async (params) => {
-        let result = [];
-        const {appModuleId, businessModelId} = this.props;
-
-        let moduleId = "";
-        if (!params.selectedKey) {
-            moduleId = appModuleId;
-        } else {
-            moduleId = params.selectedKey;
-        }
-        await listAllNotSelectEdByAppModuleId("/" + `${moduleId}/${businessModelId}`).then((res) => {
-            result = res;
-        });
-        return result;
-    };
-
-    rightService = async (params) => {
-        let result = [];
-        const {appModuleId, businessModelId} = this.props;
-        await listAllSelectEdByAppModuleId({businessModelId}).then((res) => {
-            result = res
-        });
-        return result;
-    };
-
-    JointQueryService = async (key) => {
-        const {appModuleId, businessModelId} = this.props;
-        let result = [];
-        await listAllNotSelectEdByAppModuleId("/" + `${key}/${businessModelId}`).then((res) => {
-            result = res
-        });
-        return result;
-    };
-
-    render() {
-        const {appModuleId} = this.props;
-        const leftColumns = [
-            {
-                title: seiIntl.get({key: 'flow_000022', desc: '名称'}),
-                dataIndex: 'name',
-                width: 200
-            },
-            {
-                title: seiIntl.get({key: 'flow_000033', desc: 'URL地址'}),
-                dataIndex: 'url',
-                width: 300
-            }
-        ];
-        const searchTableConfig = {...appModuleConfig, lable: seiIntl.get({key: 'flow_000041', desc: '应用模块'}), defaultValue: appModuleId,props:{canNotClear:true}};
-        const {modalVisible, handleCancel} = this.props;
-        return (
-            <Modal title={seiIntl.get({key: 'flow_000005', desc: '工作界面配置'})}
-                   visible={modalVisible}
-                   width={1000}
-                   maskClosable={false}
-                   onCancel={handleCancel}
-                   bodyStyle={{minHeight: 500}}
-                   footer={false}
-                   centered={true}
-            >
-                    <TransferTable
-                        width={900}
-                        style={{background:"#fff"}}
-                        handleLeftClick={this.handleLeftClick.bind(this)}
-                        handleRightClick={this.handleRightClick.bind(this)}
-                        searchTableConfig={searchTableConfig}
-                        rightService={this.rightService.bind(this)}
-                        leftService={this.leftService.bind(this)}
-                        JointQueryService={this.JointQueryService.bind(this)}
-                        leftColumns={leftColumns}
-                        rightColumns={leftColumns}
-                        heightY={250}
-                        leftSearch={false}
-                        rightSearch={false}
-                    />
-            </Modal>
-        );
+    for (let data of rightData) {
+      if (rows.findIndex(item => item.id === data.id) > -1) {
+        continue;
+      }
+      ids.push(data.id);
     }
+    await saveSetWorkPage(`/${businessModelId}`, ids.join(',')).then((data) => {
+    }).catch(err => {
+      this.toggoleGlobalLoading(false)
+    })
+  };
+
+  leftService = async (params) => {
+    let result = [];
+    const {appModuleId, businessModelId} = this.props;
+
+    let moduleId = "";
+    if (!params.selectedKey) {
+      moduleId = appModuleId;
+    } else {
+      moduleId = params.selectedKey;
+    }
+    await listAllNotSelectEdByAppModuleId("/" + `${moduleId}/${businessModelId}`).then((res) => {
+      result = res;
+    });
+    return result;
+  };
+
+  rightService = async (params) => {
+    let result = [];
+    const {appModuleId, businessModelId} = this.props;
+    await listAllSelectEdByAppModuleId({businessModelId}).then((res) => {
+      result = res
+    });
+    return result;
+  };
+
+  JointQueryService = async (key) => {
+    const {appModuleId, businessModelId} = this.props;
+    let result = [];
+    await listAllNotSelectEdByAppModuleId("/" + `${key}/${businessModelId}`).then((res) => {
+      result = res
+    });
+    return result;
+  };
+
+  render() {
+    const {appModuleId} = this.props;
+    const leftColumns = [
+      {
+        title: seiIntl.get({key: 'flow_000022', desc: '名称'}),
+        dataIndex: 'name',
+        width: 200
+      },
+      {
+        title: seiIntl.get({key: 'flow_000033', desc: 'URL地址'}),
+        dataIndex: 'url',
+        width: 300
+      }
+    ];
+    const searchTableConfig = {
+      ...appModuleConfig,
+      lable: seiIntl.get({key: 'flow_000041', desc: '应用模块'}),
+      defaultValue: appModuleId,
+      props: {canNotClear: true}
+    };
+
+    const {modalVisible, handleCancel} = this.props;
+    return (
+      <Modal title={seiIntl.get({key: 'flow_000005', desc: '工作界面配置'})}
+             visible={modalVisible}
+             width={1200}
+             maskClosable={false}
+             onCancel={handleCancel}
+             bodyStyle={{minHeight: 500}}
+             footer={false}
+             centered={true}
+      >
+        <TransferTable
+          width={1100}
+          style={{background: "#fff"}}
+          handleLeftClick={this.handleLeftClick.bind(this)}
+          handleRightClick={this.handleRightClick.bind(this)}
+          searchTableConfig={searchTableConfig}
+          rightService={this.rightService.bind(this)}
+          leftService={this.leftService.bind(this)}
+          JointQueryService={this.JointQueryService.bind(this)}
+          leftColumns={leftColumns}
+          rightColumns={leftColumns}
+          heightY={250}
+          searchLeftKey={['name', 'url']}
+          // leftSearch={search}
+          rightSearch={false}
+        />
+      </Modal>
+    );
+
+  }
 
 
 }
 
 const mapStateToProps = ({}) => {
-    return {};
+  return {};
 }
 
 
 export default connect(
-    mapStateToProps,
+  mapStateToProps,
 )(ConfigWorkPageModal)
 
 
