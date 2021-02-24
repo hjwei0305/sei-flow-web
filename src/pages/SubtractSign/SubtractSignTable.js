060852eefe3f367ea9afd3c85cceaefcd436cbbc
何灿坤 <cankun.he@changhong.com>
2021-02-24 11:23:21 +0800

    减签页面整体优化

--- src/pages/SubtractSign/SubtractSignTable.js
+++ src/pages/SubtractSign/SubtractSignTable.js
@@ -7,17 +7,17 @@
  */
 import React, {Component} from 'react'
 import {connect} from 'dva'
-import {Input, Modal} from 'antd';
-import { message } from 'suid';
+import {Input, Modal, Tooltip} from 'antd';
+import {message} from 'suid';
 import SimpleTable from "@/components/SimpleTable";
 import {getAllCanDelSignList, setSubtractSignExecutorList} from "./SubtractSignService";
 import HeadBreadcrumb from "@/components/breadcrumb/HeadBreadcrumb";
 import SubtractSignSelected from './SubtractSignSelected';
-import { commonUtils, } from '@/utils';
-import { seiLocale } from 'sei-utils';
+import {commonUtils,} from '@/utils';
+import {seiLocale} from 'sei-utils';
 
 const {searchListByKeyWithTag} = commonUtils;
-const { seiIntl } = seiLocale;
+const {seiIntl} = seiLocale;
 const Search = Input.Search;
 
 class SubtractSignTable extends Component {
@@ -45,7 +45,7 @@ class SubtractSignTable extends Component {
   };
 
   toggoleGlobalLoading = (loading) => {
-    const { dispatch, } = this.props;
+    const {dispatch,} = this.props;
     dispatch({
       type: 'global/updateState',
       payload: {
@@ -97,7 +97,7 @@ class SubtractSignTable extends Component {
     this.setState({selectedRows})
   };
   handleSearch = (value) => {
-    searchListByKeyWithTag(this.state.data, {keyword: value}, ["flowDefKey", "flowName", "nodeKey", "nodeName", "businessCode", "businessName"]).then(data => {
+    searchListByKeyWithTag(this.state.data, {keyword: value}, ["flowName", "nodeName", "businessCode", "executorNameList"]).then(data => {
       this.setState({data, searchValue: value});
     })
   };
@@ -113,7 +113,7 @@ class SubtractSignTable extends Component {
             <div className={'row-operator'} key={"operator" + index} onClick={(e) => {
               e.stopPropagation()
             }}>
-              <a className={'row-operator-item'}  onClick={() => {
+              <a className={'row-operator-item'} onClick={() => {
                 this.currentRecord = record;
                 this.setState({
                   selectUserModal: true,
@@ -126,52 +126,44 @@ class SubtractSignTable extends Component {
         }
       },
       {
-        title: seiIntl.get({key: 'flow_000063', desc: '流程定义key'}),
-        dataIndex: 'flowDefKey',
-        width: 200
-      },
-      {
         title: seiIntl.get({key: 'flow_000047', desc: '流程名称'}),
         dataIndex: 'flowName',
         width: 200
       },
       {
-        title: seiIntl.get({key: 'flow_000064', desc: '流程节点key'}),
-        dataIndex: 'nodeKey',
-        width: 200
-      },
-      {
-        title: seiIntl.get({key: 'flow_000065', desc: '流程节点名称'}),
+        title: seiIntl.get({key: 'flow_000048', desc: '任务名称'}),
         dataIndex: 'nodeName',
         width: 200
       },
       {
-        title: seiIntl.get({key: 'flow_000066', desc: '业务单据编号'}),
+        title: seiIntl.get({key: 'flow_000132', desc: '业务单号'}),
         dataIndex: 'businessCode',
         width: 200,
       },
       {
-        title: seiIntl.get({key: 'flow_000067', desc: '业务单据名称'}),
-        dataIndex: 'businessName',
-        width: 200,
+        title: seiIntl.get({key: 'flow_000050', desc: '执行人名称'}),
+        dataIndex: 'executorNameList',
+        width: 220
       },
       {
-        title: seiIntl.get({key: 'flow_000068', desc: '业务摘要'}),
+        title: seiIntl.get({key: 'flow_000105', desc: '工作说明'}),
         dataIndex: 'businessModelRemark',
-        width: 200,
+        width: 320,
       }
     ];
 
     //表头搜索框
     const search = () => {
       return [
-        <Search
-          key="search"
-          placeholder={seiIntl.get({key: 'flow_000069', desc: '输入名称或代码进行查询'})}
-          onSearch={value => this.handleSearch(value)}
-          style={{width: 220}}
-          allowClear
-        />
+        <Tooltip title={seiIntl.get({key: 'flow_000315', desc: '流程名称、任务名称、业务单号、执行人名称'})}>
+          <Search
+            key="search"
+            placeholder={seiIntl.get({key: 'flow_000040', desc: '输入关键字搜索'})}
+            onSearch={value => this.handleSearch(value)}
+            style={{width: 220}}
+            allowClear
+          />
+        </Tooltip>
       ]
     };
     const {searchValue, data, selectedRows} = this.state;
@@ -203,8 +195,8 @@ class SubtractSignTable extends Component {
             maskClosable={false}
           >
             <SubtractSignSelected type='checkbox' actInstanceId={this.state.currentActInstanceId}
-                             taskActKey={this.state.currentTaskActKey}
-                             selectChange={(ids) => this.selectedOne = ids}/>
+                                  taskActKey={this.state.currentTaskActKey}
+                                  selectChange={(ids) => this.selectedOne = ids}/>
           </Modal>
         </div>
       </HeadBreadcrumb>
