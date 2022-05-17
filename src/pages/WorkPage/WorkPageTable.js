fa4554eb21f837d80e523a80e321d336e57935bd
何灿坤 <cankun.he@changhong.com>
2022-05-17 13:39:46 +0800

    兼容V6版本返回判断result.success

--- src/pages/WorkPage/WorkPageTable.js
+++ src/pages/WorkPage/WorkPageTable.js
@@ -90,7 +90,7 @@ class WorkPageTable extends Component {
           delete params.id;//新增时id==="",保存可能会出错，需删除id
         this.setState({confirmLoading: true});
         save(params).then(result => {
-          if (result.status === "SUCCESS") {
+          if (result.success) {
             message.success(result.message ? result.message : seiIntl.get({key: 'flow_000025', desc: '请求成功'}));
             //刷新本地数据
             this.getDataSource({
@@ -150,7 +150,7 @@ class WorkPageTable extends Component {
         params = record.id;
         thiz.toggoleGlobalLoading(true);
         deleteCorp(params).then(result => {
-          if (result.status === "SUCCESS") {
+          if (result.success) {
             message.success(result.message ? result.message : seiIntl.get({key: 'flow_000025', desc: '请求成功'}));
             //刷新本地数据
             thiz.getDataSource({
